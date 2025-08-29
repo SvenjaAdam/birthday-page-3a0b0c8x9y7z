e184b392729ffbaa54c467e73c2b3485dbc53609
Svenja Adam <sa@syndikat7.de>
2025-08-29 18:45:11 +0200

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/birthday-page-3a0b0c8x9y7z/', // Wichtig: Dein Repo-Name
+    base: './', // Wichtig: Dein Repo-Name
     server: {
         port: 3000,
         open: true
