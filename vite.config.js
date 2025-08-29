b0af086265e8c21881d6db8d8ebf1076f41650b3
Svenja Adam <sa@syndikat7.de>
2025-08-29 19:01:52 +0200

    Clean up dependencies and fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // Wichtig: Dein Repo-Name
+    base: '/birthday-page-3a0b0c8x9y7z/',
     server: {
         port: 3000,
         open: true
